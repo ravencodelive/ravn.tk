fccbbdf822a43186623eda1fb07d8546f39b6baf
Gökhan Can <55600314+chimpdev@users.noreply.github.com>
2022-09-11 00:14:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,4 +17,5 @@ server.get('/:path', (req, res) => {
 });
 
 server.get('/', (req, res) => res.status(200).send('OK'));
+server.use('*', (req, res) => res.status(404).send("Not found."));
 setInterval(() => require('http').get('http://ravn-tk.herokuapp.com'), 300000);
